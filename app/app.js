a34d1f56a4ba2be5bac1c9c84a1baa7aa465f6eb
Magdalena Michalska <magda@MacBook-Pro-Magdalena.local>
2017-12-03 08:35:48 +0100

    first commit

---
+++ app/app.js
@@ -0,0 +1,44 @@
+var OSinfo = require('../modules/OSInfo');
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input == null) {
+		process.stdout.write('For more info, please type "help"\n');
+	};
+
+	if (input !== null) {
+		var instruction = input.toString().trim();
+
+		switch(instruction)	{
+			case 'help':
+			  process.stdout.write('Type "getOSinfo", "time" to get more info about your OS. Type "exit" to leave the app.\n');
+			break;
+			case 'getOSinfo':
+              OSinfo.print();
+            break;
+            case 'time':
+              OSinfo.formatTime();
+            break;
+			case 'exit':
+			  process.stdout.write('Quitting app!\n');
+			  process.exit();
+			break;
+			default:
+			  process.stderr.write('Wrong instruction!\n');
+			break;
+		}	
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
